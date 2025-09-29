75b7e547d40c1beab428253f758e8787282b5644
Hwanji203 <hwanji203@gmail.com>
2025-09-30 00:20:29 +0900

    Feat: Question Mark

---
+++ Assets/0001_Member/0002_Hwan/0002_Script/003_Question/Question.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Question : MonoBehaviour
+{
+    [SerializeField] private QuestionType myType;
+
+    private bool playerIn;
+
+    private void Update()
+    {
+        if (Keyboard.current.eKey.wasPressedThisFrame && playerIn == true)
+        {
+   
+            QuestionManager.Instance.OpenQuestion(myType);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerIn = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerIn = false;
+    }
+}
